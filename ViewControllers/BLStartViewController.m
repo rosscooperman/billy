c56449df5dc8808a73f4118d69fdb2d064d3a146
Ross Cooperman <ross@eastmedia.com>
2012-08-24 15:05:22 -0400

    added tour text to the split controller

--- ViewControllers/BLStartViewController.m
+++ ViewControllers/BLStartViewController.m
@@ -10,7 +10,10 @@
 #import "BLSplitCountViewController.h"
 
 
-#import "BLFixItemsViewController.h"
+#import "BLSplitBillViewController.h"
+#import "Bill.h"
+#import "LineItem.h"
+#import "Person.h"
 
 @implementation BLStartViewController
 
@@ -20,7 +23,40 @@
 - (void)viewDidAppear:(BOOL)animated
 {
   //BLSplitCountViewController *countController = [[BLSplitCountViewController alloc] init];
-  BLFixItemsViewController *countController = [[BLFixItemsViewController alloc] init];
+  
+  Bill *bill = [BLAppDelegate appDelegate].currentBill;
+  
+  NSManagedObjectContext *context = [BLAppDelegate appDelegate].managedObjectContext;
+  NSEntityDescription *entity = [NSEntityDescription entityForName:@"LineItem" inManagedObjectContext:context];
+  NSMutableSet *lineItems = [[NSMutableSet alloc] initWithCapacity:2];
+  NSMutableSet *people = [[NSMutableSet alloc] initWithCapacity:bill.splitCount];
+  
+  LineItem *item = [[LineItem alloc] initWithEntity:entity insertIntoManagedObjectContext:context];
+  item.quantity = 1;
+  item.desc = @"FIRST ITEM";
+  item.price = 10.50;
+  [lineItems addObject:item];
+  
+  item = [[LineItem alloc] initWithEntity:entity insertIntoManagedObjectContext:context];
+  item.quantity = 3;
+  item.desc = @"SECOND ITEM";
+  item.price = 22.95;
+  [lineItems addObject:item];
+  
+  NSEntityDescription *personEntity = [NSEntityDescription entityForName:@"Person" inManagedObjectContext:context];
+  for (int i = 0; i < bill.splitCount; i++) {
+    Person *person = [[Person alloc] initWithEntity:personEntity insertIntoManagedObjectContext:context];
+    person.name = @"ROSS";
+    person.index = i;
+    [people addObject:person];
+  }
+  
+  [bill addLineItems:lineItems];
+  [bill addPeople:people];
+  [context save:nil];
+  
+  BLSplitBillViewController *countController = [[BLSplitBillViewController alloc] init];
+  
   [self.navigationController pushViewController:countController animated:YES];
 }
 
